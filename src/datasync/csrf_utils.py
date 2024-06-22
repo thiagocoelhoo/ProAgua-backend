50801bf12ded555311f3f7accaf11fd067a961eb
Thiago Coelho <thiago.coelho158@gmail.com>
2024-06-21 23:22:02 -0300

    remove: deletar arquivos desnecessarios para deploy

--- src/datasync/csrf_utils.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def obter_token_csrf(url_csrf):
-    response = requests.get(url_csrf)
-    response.raise_for_status()  # Lançando uma exceção se a resposta não for bem-sucedida
-    return response.cookies['csrftoken']
