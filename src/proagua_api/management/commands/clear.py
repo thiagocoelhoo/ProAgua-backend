1a853212e4a720b4dfd84d295e9dc2b20156471f
vynijales <vynijales@gmail.com>
2024-06-25 18:30:26 -0300

    feat: tornar o comando clear compatível com o PostgreSQL

--- src/proagua_api/management/commands/clear.py
+++ src/proagua_api/management/commands/clear.py
@@ -2,36 +2,20 @@ import os
 import shutil
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.db import connection
 
 class Command(BaseCommand):
-    help = 'Reset migrations and delete the SQLite database file.'
+    help = 'Reset migrations and delete the PostgreSQL database'
 
     def handle(self, *args, **options):
-        migrations_path = os.path.join('src', 'apps', 'proagua_api', 'migrations')
-        db_file = 'src/db.sqlite3'
-
-        confirmation = input("Are you sure you want to reset the migrations and delete the database? [y/N]: ")
-
-        if confirmation.lower() != "y":
-            self.stdout.write(self.style.SUCCESS("Operação cancelada. Nada foi alterado."))
-            return
-
-        # Deletar arquivos de migração
+        migrations_path = os.path.join('src', 'proagua_api', 'migrations')
         if os.path.exists(migrations_path):
             shutil.rmtree(migrations_path)
             self.stdout.write(self.style.SUCCESS("Pasta migrations excluída com sucesso."))
+        with connection.cursor() as cursor:
 
-        else:
-            self.stdout.write(self.style.ERROR("Pasta migrations não encontrada."))
-
-        # Deletar o arquivo do banco de dados SQLite3
-        if os.path.exists(db_file):
-            os.remove(db_file)
-            self.stdout.write(self.style.SUCCESS("Banco de dados deletado com sucesso."))
-            
-        else:
-            self.stdout.write(self.style.ERROR("Banco de dados não encontrado."))
-
+            cursor.execute('DROP SCHEMA public CASCADE; CREATE SCHEMA public;')
+            self.stdout.write(self.style.SUCCESS("Banco de dados PostgreSQL deletado com sucesso."))
 
 if __name__ == '__main__':
     command = Command()
